4c5a62458a9368fdea48afd0610fcf047d60e769
¨Carlos <carlosrobertovelasquez@gmail.com>
2017-01-31 12:06:36 -0600

    Cambio de CCS

--- resources/views/store/partials/footer.blade.php
+++ resources/views/store/partials/footer.blade.php
@@ -1,9 +1,10 @@
-<div class="container-fluid">
+<footer class="container-fluid">
 	<div class="row">
 		<div class="col-md-4">
 			<h3> My Laravel Store</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere quisquam, porro dolore fuga repellat. Ad perspiciatis sed modi architecto, numquam, voluptas placeat, blanditiis rem quaerat, cupiditate at consequuntur. Ex odio minima natus facilis, cumque aperiam, animi harum illo, aut beatae temporibus voluptate repudiandae ipsa a vero exercitationem, voluptas in laborum. </p>
 		</div>
+		
 		<div class="col-md-4">
 			<h3> Desarrollado Por :</h3>
 			<div class="autor-info">
@@ -11,17 +12,30 @@
 				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim laborum non deleniti consequatur ab eos, consectetur cupiditate dolorem molestias. Ipsum enim maiores debitis nobis voluptate pariatur architecto sapiente doloribus voluptatibus ipsam cumque voluptates aliquid itaque dicta deserunt nostrum iusto, quo atque, nesciunt quasi. Est eaque, perferendis perspiciatis, expedita ipsum ad.</p>
 			</div>
 		</div>
-		<div class="col-md-4">
-			<h3>Siguemos</h3>
-			<ul class="redes">
-				<li>
-					<a href="#"><i class="fa fa-facebook-square fa-2x"></a>
-				</li>
-				
-				
-			</ul>
-			<h3>Escribenos:</h3>
-		  <i class="fa fa-at"></i> <a href="#">dfer@gmail.com</a>
+	
+        <div class="col-md-4">
+			
+			<div class="autor-info">
+				<h3> Siguemos :</h3>
+				<ul class="redes"> 
+                   <li>
+                     <a href=""><i class="fa fa-facebook-square fa-2x"></i></a>
+                   </li>
+                   <li>
+                     <a href=""><i class="fa fa-twitter-square fa-2x"></i></a>
+                   </li>
+                   <li>
+                     <a href=""><i class="fa fa-google-plus-square fa-2x"></i></a>
+                   </li>
+                   <li>
+                     <a href=""><i class="fa fa-linkedin-square fa-2x"></i></a>
+                   </li>
+                   <li>
+                     <a href=""><i class="fa fa-youtube-square fa-2x"></i></a>
+                   </li>
+				</ul>
+				<h3>Escribanos</h3>
+				<i class="fa fa-at"></i><a href="">cvelasquez@gmail.com</a>
+			</div>
 		</div>
-	</div>
-</div>
+</footer>
